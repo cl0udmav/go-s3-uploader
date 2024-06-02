5ea9feb0fa9add3f85a82d063509c19fa79fb567
awakenthereas0n <lc1816@gmail.com>
2024-06-01 21:31:47 -0700

    skip files already uploaded to s3

--- main.go
+++ main.go
@@ -6,19 +6,17 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
 )
 
 var (
-	lastRuntime time.Time
-	localPath   string
-	bucket      string
-	prefix      string
+	localPath string
+	bucket    string
+	prefix    string
 )
 
 func init() {
@@ -31,80 +29,82 @@ func init() {
 }
 
 func main() {
-	// Create a local cache file to check
-	// last script runtime date
-	lastRuntimeFilePath := filepath.Join(localPath, "._S3UploaderLastRuntime")
-	lastRuntimeFileInfo, err := os.Stat(lastRuntimeFilePath)
-	if err == nil {
-		lastRuntime = lastRuntimeFileInfo.ModTime()
-	} else {
-		lastRuntime = time.Time{}
+	// Gather the files to upload by walking the path recursively
+	var files []string
+	if err := filepath.Walk(localPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// Exclude directories
+		if info.IsDir() {
+			return nil
+		}
+		// Exclude filenames that start with "._"
+		if strings.HasPrefix(info.Name(), "._") {
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	}); err != nil {
+		log.Fatalln("Walk failed:", err)
 	}
 
-	walker := make(fileWalk)
-	go func() {
-		// Gather the files to upload by walking the path recursively
-		if err := filepath.Walk(localPath, walker.Walk); err != nil {
-			log.Fatalln("Walk failed:", err)
-		}
-		close(walker)
-	}()
-
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	cfg, err := config.LoadDefaultConfig(context.Background())
 	if err != nil {
 		log.Fatalln("error:", err)
 	}
 
-	// For each file found walking, upload it to Amazon S3
-	uploader := manager.NewUploader(s3.NewFromConfig(cfg))
-	for path := range walker {
+	// Create S3 client
+	client := s3.NewFromConfig(cfg)
+
+	// Create S3 uploader
+	uploader := manager.NewUploader(client)
+
+	// List all objects in the bucket
+	listObjectsOutput, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(prefix),
+	})
+	if err != nil {
+		log.Fatalf("Failed to list objects in bucket: %v", err)
+	}
+
+	// Store object keys in a map for quick lookup
+	existingKeys := make(map[string]struct{})
+	for _, object := range listObjectsOutput.Contents {
+		existingKeys[*object.Key] = struct{}{}
+	}
+
+	// For each file found walking, upload it to Amazon S3 if it's not already present
+	for _, path := range files {
 		rel, err := filepath.Rel(localPath, path)
 		if err != nil {
 			log.Fatalln("Unable to get relative path:", path, err)
 		}
+		objectKey := filepath.Join(prefix, rel)
+		// Check if the object key already exists in S3
+		if _, ok := existingKeys[objectKey]; ok {
+			log.Printf("Skipped upload for %s - Already exists in S3\n", path)
+			continue
+		}
+
+		// Open the file
 		file, err := os.Open(path)
 		if err != nil {
 			log.Println("Failed opening file", path, err)
 			continue
 		}
 		defer file.Close()
-		result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-			Bucket:       &bucket,
-			Key:          aws.String(filepath.Join(prefix, rel)),
-			Body:         file,
-			StorageClass: "GLACIER",
+
+		// Upload the file to S3
+		_, err = uploader.Upload(context.TODO(), &s3.PutObjectInput{
+			Bucket: &bucket,
+			Key:    &objectKey,
+			Body:   file,
 		})
 		if err != nil {
 			log.Fatalln("Failed to upload", path, err)
 		}
-		log.Println("Uploaded", path, result.Location)
+		log.Println("Uploaded", path, "to", objectKey)
 	}
-
-	currentTimeBytes := []byte(time.Now().UTC().String())
-	os.WriteFile(lastRuntimeFilePath, currentTimeBytes, 0755)
-}
-
-type fileWalk chan string
-
-func (f fileWalk) Walk(path string, info os.FileInfo, err error) error {
-	// Exclude directories
-	if info.IsDir() {
-		return nil
-	}
-
-	// Exclude filenames that start with "._"
-	if strings.HasPrefix(info.Name(), "._") {
-		return nil
-	}
-
-	// Exclude files that are already up-to-date in S3
-	// We know they're up-to-date if the last script runtime
-	// file hasn't been modified since the last script run
-	if info.ModTime().Before(lastRuntime) {
-		return nil
-	}
-
-	// Add path to fileWalk
-	f <- path
-	return nil
 }
